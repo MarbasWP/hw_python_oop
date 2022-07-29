366f3585bf4ecfd5f79740dbf47fbe872732227f
Ilya <ilya_stezhko@mail.ru>
2022-07-29 10:57:22 +0300

    Split2_Final_Task_Attempt_3

--- homework.py
+++ homework.py
@@ -10,11 +10,11 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    PHRASE_INFO = ('Тип тренировки: {training_type}; '
-                   'Длительность: {duration:.3f} ч.; '
-                   'Дистанция: {distance:.3f} км; '
-                   'Ср. скорость: {speed:.3f} км/ч; '
-                   'Потрачено ккал: {calories:.3f}. '
+    PHRASE_INFO = ('Тип тренировки: {training_type};'
+                   'Длительность: {duration:.3f} ч.;'
+                   'Дистанция: {distance:.3f} км;'
+                   'Ср. скорость: {speed:.3f} км/ч;'
+                   'Потрачено ккал: {calories:.3f}.'
                    )
 
     def get_message(self) -> str:
